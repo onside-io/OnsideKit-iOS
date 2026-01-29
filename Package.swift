4430487b2940aa351ef2afa940f5370b55eedc08
Alexander Goremykin <ifresh.wp@gmail.com>
2026-01-29 18:18:37 +0100

    0.4.0

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "OnsideKit",
-            url: "https://github.com/onside-io/OnsideKit-iOS/releases/download/0.3.0/OnsideKit.xcframework.zip",
-            checksum: "0adc1756082a7e52c50661e75dd1cb6358c6f8ced49523f67fbaca731d75a707"
+            url: "https://github.com/onside-io/OnsideKit-iOS/releases/download/0.4.0/OnsideKit.xcframework.zip",
+            checksum: "7c2e21b92dd63b4b5ff65bc5a1c8798530791e1ad056dc4fcd8842c19ce42510"
         )
     ]
 )
